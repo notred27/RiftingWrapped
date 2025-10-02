5d64038a5ddc6190951481823c87ac134ea1af4e
Michael Reidy <mikereidy95@gmail.com>
2025-10-02 00:14:51 -0400

    Adding kofi and logo

--- src/Home.js
+++ src/Home.js
@@ -123,9 +123,12 @@ function Home() {
             </Helmet>
 
             <header className="siteHeader">
-                <div className="logo">
-                    Rifting Wrapped 2025
-                </div>
+                <span style={{display:"flex", gap:"5px"}}>
+                    <img src='favicon-32x32.png' alt='rifting wrapped logo'></img>
+                    <div className="logo">
+                        Rifting Wrapped 2025
+                    </div>
+                </span>
                 <nav className="navMenu">
                     <Link to="/">Home</Link>
                     <Link to="/faq">FAQ</Link>
@@ -187,6 +190,10 @@ function Home() {
 
 
             <p id='FooterNote'>All data used in Rifting Wrapped comes from the public League of Legends matches a user has participated in. Rifting Wrapped isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.</p>
+
+
+
+
         </>
     );
 }
