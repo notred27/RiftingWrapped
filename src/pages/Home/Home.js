f5adbad9801fad313f4ec4afbb3990f391b7e765
notred27 <mikereidy95@gmail.com>
2025-12-15 13:15:56 -0500

    Update home css and adding player error msg logic

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -18,7 +18,7 @@ import GenericSearch from '../../components/common/GenericSearch';
 function Home() {
     const navigate = useNavigate();
 
-    const [numUsers, setNumUsers] = useState(30);
+    const [numUsers, setNumUsers] = useState(140);
     const [selectedPlayer, setSelectedPlayer] = useState('');
     const [isLoading, setIsLoading] = useState(false);
 
@@ -28,65 +28,75 @@ function Home() {
             .then((res) => setNumUsers(res.count))
     }, [])
 
+
+
     const fetchPlayer = async (e) => {
-        const apiUrl = process.env.REACT_APP_API_ENDPOINT;
         e.preventDefault();
         setIsLoading(true);
         setSelectedPlayer("");
 
+        const apiUrl = process.env.REACT_APP_API_ENDPOINT;
+
         try {
             const region = document.getElementById("regionSelect").value;
             const names = document.getElementById("nameInput").value.split("#");
+
             if (names.length !== 2) {
                 setSelectedPlayer("Invalid Search Name");
                 setIsLoading(false);
                 return;
             }
 
-            const displayName = names[0];
-            const tag = names[1];
+            const [displayName, tag] = names;
             setSelectedPlayer(displayName);
 
-            let response = await fetch(`${apiUrl}/users/by-riot-id/${displayName}/${tag}/${region}`);
+            let response = await fetch(
+                `${apiUrl}/users/by-riot-id/${displayName}/${tag}/${region}`
+            );
 
             if (response.status === 404) {
-                console.log("User not found, trying to add...");
-
                 const addResponse = await fetch(`${apiUrl}/users`, {
                     method: "POST",
                     body: new URLSearchParams({ displayName, tag, region }),
                 });
 
                 if (!addResponse.ok) {
-                    throw new Error("Failed to add new user");
+                    setSelectedPlayer("Failed to find user. Please check that your name and region are correct.");
+                    setIsLoading(false);
+                    return;
                 }
 
-                // Fetch again to get puuid
-                response = await fetch(`${apiUrl}/users/by-riot-id/${displayName}/${tag}`);
+                // try to find user after init
+                response = await fetch(
+                    `${apiUrl}/users/by-riot-id/${displayName}/${tag}/${region}`
+                );
             }
 
+
             if (!response.ok) {
-                throw new Error("API Error when fetching player data");
+                setSelectedPlayer("User could not be fetched after creation.");
+                setIsLoading(false);
+                return;
             }
 
             const result = await response.json();
+            const { puuid, status } = result;
 
-            const puuid = result["puuid"];
-            const status = result["status"];
-
-            if (status === "done") {
-                navigate(`/player/${puuid}`);
-            } else {
-                navigate(`/addPlayer/${puuid}`);
-            }
-
-            setIsLoading(false);
-        } catch (error) {
-            console.error(error);
-            setSelectedPlayer("Error fetching or adding user. Please check the entered information and try again.");
+            navigate(
+                status === "done"
+                    ? `/player/${puuid}`
+                    : `/addPlayer/${puuid}`
+            );
+        } catch (err) {
+            console.error(err);
+            setSelectedPlayer(
+                "Unexpected error. Please try again later."
+            );
+        } finally {
             setIsLoading(false);
         }
     };
+
 
 
 
@@ -107,52 +117,64 @@ function Home() {
 
             </Helmet>
 
-            <div className="heroContainer" >
+            <div className="heroContainer"  >
                 <img className="heroOverlay" src={bg_image} alt="Hero Overlay" />
-                <div className="heroText">
-                    <h1>Your Year on the Rift, <span style={{ fontWeight: "bolder", fontStyle: "italic" }}>Unwrapped.</span></h1>
-                    <p>
-                        Discover your top champions, match stats, and trends for 2025 with a personalized recap of your League of Legends journey.
-                    </p>
+                <div className="heroText" style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "0px 30px", flexWrap: "wrap" }}>
+                    <div style={{ width: "600px", textAlign: "left" }}>
+
+                        <h1>Your Year on the Rift, <span style={{ fontWeight: "bolder", fontStyle: "italic" }}>Unwrapped.</span></h1>
+                        <p>
+                            Discover your top champions, stats, and trends of <strong>2025</strong> with a personalized recap of your <strong>League of Legends</strong> journey.
+                        </p>
+                    </div>
 
 
-                    <form onSubmit={fetchPlayer} className="searchForm">
-                        <span style={{ display: "flex", gap: "10px", alignItems: "center", width:"100%"}}>
+                    <div>
 
-                            <PlayerListProvider>
-                                <ErrorBoundary fallback={(err) => <GenericSearch error={err} />}>
-                                    <Suspense fallback={<GenericSearch />}>
-                                        <UserSearchBar />
-                                    </Suspense>
-                                </ErrorBoundary>
-                            </PlayerListProvider>
+                        <form onSubmit={fetchPlayer} className="searchForm">
+                            <span style={{ display: "flex", gap: "10px", alignItems: "center", width: "100%" }}>
 
-                        </span>
+                                <PlayerListProvider>
+                                    <ErrorBoundary fallback={(err) => <GenericSearch error={err} />}>
+                                        <Suspense fallback={<GenericSearch />}>
+                                            <UserSearchBar />
+                                        </Suspense>
+                                    </ErrorBoundary>
+                                </PlayerListProvider>
 
-                        <button type="submit" id="submitBtn">
-                            Fetch My Stats!
-                        </button>
-                    </form>
-
-
-                    {selectedPlayer && !isLoading && <p className="search-error">{selectedPlayer}</p>}
-                    {isLoading &&
-
-                        <h2 className="loading-text">
-                            Loading {selectedPlayer}'s stats<span className="dots">
-                                <span>.</span>
-                                <span>.</span>
-                                <span>.</span>
                             </span>
-                        </h2>
 
-                    }
+                            <button type="submit" id="submitBtn">
+                                Fetch My Stats!
+                            </button>
+                        </form>
+
+
+                        {selectedPlayer && !isLoading && <p className="search-error" style={{maxWidth:"400px", fontSize:"small"}}>{selectedPlayer}</p>}
+                        {isLoading &&
+
+                            <p className="loading-text">
+                                Loading {selectedPlayer}'s stats<span className="dots">
+                                    <span>.</span>
+                                    <span>.</span>
+                                    <span>.</span>
+                                </span>
+                            </p>
+
+                        }
+                    </div>
+
                 </div>
 
-                <h2 style={{ marginTop: "40px", maxWidth: "800px", fontSize: "large" }}>Don't see your name? Enter your account and region to join over <span className='emphasize'>{numUsers}</span> other users in tracking your yearly LOL metrics!</h2>
+                <PlayerMarquee />
+
+
+
+                <h3 style={{ margin: "0px", maxWidth: "1000px", fontSize: "16px", fontWeight: "initial" }}>
+                    Don't see your username? Join over <span className='emphasize' style={{ fontWeight: "bold", textDecoration: "underline" }}><i>{numUsers} players</i></span> in tracking your yearly <strong>LOL</strong> metrics!
+                </h3>
 
                 {/* <PlayerListProvider> */}
-                <PlayerMarquee />
 
                 {/* </PlayerListProvider> */}
             </div>
